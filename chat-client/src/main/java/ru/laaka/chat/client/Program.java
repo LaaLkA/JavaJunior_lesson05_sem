55324d671020142796ed85b079266df8dd1957fc
LaaLkA <n.afonchenkov@yandex.ru>
2024-10-31 16:10:03 +0300

    Initial commit

---
+++ chat-client/src/main/java/ru/laaka/chat/client/Program.java
@@ -0,0 +1,7 @@
+package ru.laaka.chat.client;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Server client...");
+    }
+}
---
+++ chat-server/src/main/java/ru/laalka/chat/server/Program.java
@@ -0,0 +1,8 @@
+package ru.laalka.chat.server;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Server start...");
+    }
+}
