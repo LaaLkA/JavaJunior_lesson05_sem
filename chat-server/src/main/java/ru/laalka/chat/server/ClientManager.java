1e43b6b7e15a8ddfd3f66b9b511957b99e9a57da
LaaLkA <n.afonchenkov@yandex.ru>
2024-11-17 13:28:46 +0300

    Compl hw5

--- chat-server/src/main/java/ru/laalka/chat/server/ClientManager.java
+++ chat-server/src/main/java/ru/laalka/chat/server/ClientManager.java
@@ -14,25 +14,74 @@ public class ClientManager implements Runnable {
     public final static ArrayList<ClientManager> clients = new ArrayList<>();
 
     public ClientManager(Socket socket) {
+        this.socket = socket;
         try {
-            this.socket = socket;
             bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             name = bufferedReader.readLine();
             clients.add(this);
             System.out.println(name + " подключился к чату.");
-
+            broadcastMessage("SERVER: " + name+ " подключился к чату");
         } catch (IOException e) {
-
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
     @Override
     public void run() {
         String messageFromClient;
-
         while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                // for macOS
+                if (messageFromClient == null) {
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+    private boolean checkPrivateMessage(String message){
+        String[] messageParts = message.split(" ");
+        return messageParts[1].startsWith("@");
+    }
 
+    private void privateMassage(String message){
+        String[] messageParts = message.split(" ");
+        messageParts[1] = messageParts[1].substring(1);
+        for(ClientManager client: clients){
+            try{
+                if (client.name.equals(messageParts[1])){
+                    client.bufferedWriter.write(message);
+                    client.bufferedWriter.newLine();
+                    client.bufferedWriter.flush();
+                }
+            }catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    private void broadcastMessage(String message) {
+        System.out.println(message);
+        if (checkPrivateMessage(message)) {
+            privateMassage(message);
+        } else {
+            for (ClientManager client : clients) {
+                try {
+                    if (!client.name.equals(name)) {
+                        client.bufferedWriter.write(message);
+                        client.bufferedWriter.newLine();
+                        client.bufferedWriter.flush();
+                    }
+                } catch (IOException e) {
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                }
+            }
         }
     }
 
@@ -52,4 +101,12 @@ public class ClientManager implements Runnable {
             e.printStackTrace();
         }
     }
+
+    private void removeClient() {
+        clients.remove(this);
+        System.out.println(name + " покинул чат.");
+        broadcastMessage("SERVER: " + name + " покинул чат");
+    }
+
+
 }
